110bcfa60d12e23407a45fd3427b25923bf46bb8
JanisKristsPlatais <154247561+JanisKristsPlatais@users.noreply.github.com>
2024-01-20 20:10:28 +0200

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,9 +7,6 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-	public function __construct(){
-		$this->middleware('auth');
-	}
 	
     public function index(){
 		return view('public', ['title' => 's22_platjani']);
